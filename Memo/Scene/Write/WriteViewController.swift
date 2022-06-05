f8965d7602250e83cc2fe1151540ada04a9fc5b5
Youngjae Choi <cdw0807@naver.com>
2022-06-05 23:57:34 +0900

    메모작성 화면 네비게이션 버튼 구현

--- Memo/Scene/Write/WriteViewController.swift
+++ Memo/Scene/Write/WriteViewController.swift
@@ -8,11 +8,46 @@
 import UIKit
 
 class WriteViewController: UIViewController {
+    // 메모 작성 뷰
+    private let textView: UITextView = {
+        let textView = UITextView()
+        textView.becomeFirstResponder()
+        return textView
+    }()
+    
+    // 오른쪽 바 버튼 : 잠금버튼
+    private lazy var lockRightBarButton = UIBarButtonItem (
+        image: UIImage(systemName: "lock"),
+        style: .done,
+        target: self,
+        action: nil
+    )
+    
+    // 오른쪽 바 버튼 : 작성 완료 버튼
+    private lazy var saveRightBarButton = UIBarButtonItem (
+        image: UIImage(systemName: "checkmark"),
+        style: .done,
+        target: self,
+        action: nil)
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .blue
+    private func applyNavBar() {
+        navigationItem.rightBarButtonItems = [
+            lockRightBarButton, saveRightBarButton
+        ]
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        
+        view.addSubview(textView)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        title = "메모 작성"
+        
+        textView.frame = view.bounds
+    }
 
 }
